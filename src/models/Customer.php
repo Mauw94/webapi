e5c86bb34fa9a84ea9fab4b4a4a62770301a9dd6
Maurits Seelen <mauritsseelen@gmail.com>
2017-05-25 13:28:53 +0200

    repository added, controller added, findbyId method added, views implemented

---
+++ src/models/Customer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Maurits
+ * Date: 25-5-2017
+ * Time: 13:19
+ */
+
+namespace models;
+
+
+class Customer
+{
+
+}
---
+++ src/models/PDOCustomerRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Maurits
+ * Date: 25-5-2017
+ * Time: 13:14
+ */
+class PDOCustomerRepository
+{
+
+}
